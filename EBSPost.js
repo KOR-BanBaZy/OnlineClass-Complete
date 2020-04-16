cf59420473b155c691b3ce43f843c30243ef1330
keli-kor <developejimin@naver.com>
2020-04-16 12:29:44 +0900

    POST 오류 FIX

---
+++ EBSPost.js
@@ -0,0 +1,24 @@
+/**/
+
+function DoIt(){
+   // videoTime is string type.
+    const lctreSn = Number($("#lctreSn").val())
+    let atn = $("#atnlcNo").val();
+    let step = $("#stepSn").val();
+    let sc = $("#lctreSeCode").val();
+    let ct = $("#cntntsTyCode").val();
+    let AllowTime = timeToSeconds($(".vjs-duration-display").val());
+    let headers = {}
+    headers.atnlcNo=atn;
+    headers.lctreSn = lctreSn;
+    headers.stepSn = step;
+    headers.lctreSeCode = se;
+    headers.cntntsTyCode = ct;
+    headers.revivTime = AllowTime;
+    headers.endButtonYn = "Y";
+    headers.lastRevivLc = AllowTime;
+    headers.lrnTime = AllowTime;
+    fncPost("/mypage/userlrn/lctrernSave.do", headers, function(){
+           console.log("Post OK!")
+    });
+}    
